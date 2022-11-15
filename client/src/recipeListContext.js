b61dfd5c62eb084f810e3ab83bb8cff318e45323
Bradley Abbitt <abbittbradley@gmail.com>
2022-11-15 13:24:36 -0700

    Working on merge

--- client/src/recipeListContext.js
+++ client/src/recipeListContext.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
 
 const RecipeListContext = React.createContext();
 
